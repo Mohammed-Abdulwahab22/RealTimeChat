f86b99c02b417311bcfdb8c86b2131ac3c6920fd
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2024-09-18 08:51:52 +0300

    Styled Home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { NavBar } from './NavBar'
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
     return (
         <div className='Home-container'>
-            Home
+        <NavBar />
+        <div className='Home-content'>
+            <img src="src\assets\App-people-chatting.png" alt="logo" height={150} />
+            <h1>Your World, Your Conversations</h1>
+            <p>
+                Talky is more than just a chat app; it's your personal communication hub. Connect seamlessly with friends, family, and loved ones, no matter where they are. Share your thoughts, experiences, and moments in real-time, fostering deeper connections and building lasting memories. Discover new communities, join exciting discussions, and find your people. With Talky, communication is effortless, engaging, and enriching. Experience the future of chatting today.
+            </p>
+            <Link to="/register" className="Home-button">Get Started</Link>
         </div>
+    </div>
     )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    return (
+        <div className='NavBar'>
+            <div className='NavBar-logo'>
+            <img src="src/assets/App-logo.png" alt="logo" height={50} />
+            <span className='NavBar-title'>Talky</span>
+            </div>
+            <div className='NavBar-links'>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+                <Link to="/">Contact Us</Link>
+             </div>
+        </div>
+    )
 }
