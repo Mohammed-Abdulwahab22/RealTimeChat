d87aa552eb41d3818d49dc5f212a288b440e6128
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2024-09-20 08:54:58 +0300

    styled not found page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-    return <h1>404 - Page Not Found</h1>;
+    return (
+        <div className="not-found-container">
+            <h1 className="error-code">404</h1>
+            <p className="error-message">Oops! Page not found.</p>
+            <p className="error-description">It seems we can't find the page you're looking for.</p>
+            <Link to="/" className="home-button">Go to Homepage</Link>
+        </div>
+    );
 };
 
 export default NotFound;
--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const ProfilePage = () => {
   return (
-    <div></div>
+    <div>profile page</div>
   )
 }
